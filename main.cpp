b8cc43aeb0265d2b8981c4b2365b7758f2bd6454
Muntaha _Salim <muntahasalimabd@gmail.com>
2026-03-28 17:37:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,6 @@
-
 #include <iostream>
 #include <cmath>
 using namespace std;
-
-//added by Fatima Ali
-void toBinary(int num) {
-    int binary[32], i = 0;
-
-    int n = abs(num);
-
-    while (n > 0) {
-        binary[i] = n % 2;
-        n = n / 2;
-        i++;
-    }
-
-    cout << "Binary: ";
-    for (int j = i - 1; j >= 0; j--) {
-        cout << binary[j];
-    }
-    cout << endl;
-}
 
 void checkNumber(int num)
 {
@@ -36,11 +16,46 @@ void checkNumber(int num)
     else
         cout << "Odd number\n";
 }
-
 void calculate(int num)
 {
     cout << "Square: " << num * num << endl;
     cout << "Square root: " << sqrt(abs(num)) << endl;
+}
+
+// Added by: Fatima Ali
+// Feature: Convert number to Binary
+void toBinary(int num) {
+    int binary[32], i = 0;
+    int n = abs(num);
+
+    while (n > 0) {
+        binary[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    cout << "Binary: ";
+    for (int j = i - 1; j >= 0; j--) {
+        cout << binary[j];
+    }
+    cout << endl;
+}
+// Added by:muntaha salem
+// Feature: Check if number is Prime
+void checkPrime(int num) {
+    if (num <= 1) {
+        cout << "Not a prime number\n";
+        return;
+    }
+
+    for (int i = 2; i < num; i++) {
+        if (num % i == 0) {
+            cout << "Not a prime number\n";
+            return;
+        }
+    }
+
+    cout << "Prime number\n";
 }
 
 int main()
@@ -53,6 +68,7 @@ int main()
     checkNumber(number);
     calculate(number);
     toBinary(number);
+    checkPrime(number);
 
     return 0;
 }
